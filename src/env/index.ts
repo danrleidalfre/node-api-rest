b99b62ce434b1484ce2e6429f2158fc78ba8f9ec
Danrlei <danrleidalfre@gmail.com>
2023-11-26 15:29:41 -0300

    implement cookie session and define type database props

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,7 +6,6 @@ const envSchema = z.object({
     .enum(['development', 'test', 'production'])
     .default('development'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
-import { env } from './env'
 import { fastify } from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
+import { transactionsRoutes } from './routes/transactions'
+import cookie from '@fastify/cookie'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  return await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transaction',
-    amount: 2000,
-  })
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
-app.listen({
-  port: env.PORT,
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Running Server')
+  })
